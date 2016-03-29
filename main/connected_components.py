c7bad93dbf59af38baf85bd9808a95b6955b59c6
Jeroen van de Ven <Jeroen van de Ven>
2016-03-29 11:01:01 +0200

    Simplify

--- main/connected_components.py
+++ main/connected_components.py
@@ -8,16 +8,14 @@ def components():
     c.execute("select `id` from authors order by rand()")
     nodes = set([item[0] for item in c.fetchall()])
     c.execute("SET GLOBAL general_log = 'OFF';")
-    cnum = 0;
+    cnum = 0
 
     # Loop until all the nodes have been processed
     while len(nodes) > 0:
         node = nodes.pop()
         component = {node}
-        format_strings = ','.join(['%s'] * len(component))
         try:
-            c.execute("select `out` from edges2 where `in` in (%s)" % format_strings + " and `out` not in (%s)" % format_strings, tuple(2 * list(component)))
-            #c.execute("select `out` from edges2 where FIND_IN_SET(`in`,@list) > 0 and FIND_IN_SET(`out`,@list) < 1")
+            c.execute("select `out` from edges2 where `in` = '" + str(node) + "'")
         except:
             print(c._last_executed)
             raise
@@ -28,7 +26,6 @@ def components():
             component |= bfs
             format_strings = ','.join(['%s'] * len(component))
             try:
-                #c.execute("set @list = '%s'" % format_strings, tuple(component))
                 c.execute("select `out` from edges2 where `in` in (%s)" % format_strings + " and `out` not in (%s)" % format_strings, tuple(2 * list(component)))
             except:
                 print(c._last_executed)
